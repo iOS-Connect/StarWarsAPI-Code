78de2e4369605eaad29949b290a72fada231113b
yhu98 <yhu98@ucsc.edu>
2016-10-09 15:13:56 -0700

    added segment control inside navigationItem.titleView

--- StarWarsAPI-Code/NamesTableViewController.swift
+++ StarWarsAPI-Code/NamesTableViewController.swift
@@ -3,15 +3,27 @@ import UIKit
 class NamesTableViewController: UITableViewController {
 
     var names = ["Douglas", "Jay", "Gloria", "John"]
+    var tableSegments = ["Names", "Spaceships"]
     let nameCell = "NameCell"
 
+
     override func viewDidLoad() {
-        title = "Star Wars Names"
+//        title = "Star Wars Names"
+        
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: nameCell)
         Network.getNames { [weak self] (names) in
             self?.names = names
             self?.tableView.reloadData()
         }
+        let segmentControl = UISegmentedControl(items: tableSegments)
+        segmentControl.selectedSegmentIndex = 0
+        navigationController?.navigationBar.backgroundColor = UIColor.blue
+        segmentControl.addTarget(self, action: #selector(NamesTableViewController.segmentDidChange(sender:)), for: .valueChanged)
+        navigationItem.titleView = segmentControl
+    }
+    
+    func segmentDidChange(sender:UISegmentedControl) {
+        print("segment \(sender.selectedSegmentIndex) selected")
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
